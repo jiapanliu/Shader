23aa4c364b69a044ea597d239a71f5f9a288af56
LJpan <979581164@qq.com>
2023-09-11 19:56:06 +0800

    上传PaintShader工程

---
+++ PaintShader/Assets/Master/Painter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Painter : MonoBehaviour
+{
+    public Camera cam;
+    [Space]
+    public bool mouseSingleClick;
+    [Space]
+    public Color paintColor;
+
+    [Min(0.5f)]
+    public float radius = 1;
+    public float strength = 1;
+    public float hardness = 1;
+}
+
---
+++ PaintShader/Assets/_Scripts/MonoSingleton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+
+    private static T _instance;
+
+	public static T instance{
+		get{
+			if(_instance==null){
+				GameObject obj=new GameObject(typeof(T).Name);
+				_instance=obj.AddComponent<T>();
+			}
+			return _instance;
+		}
+	}
+
+	protected virtual void Awake(){
+		_instance=this as T;
+	}
+}
